14f92bbeb658440b953544732476db99763abdfe
Diego <yeogalindo@gmail.com>
2025-10-30 11:59:16 -0500

    Add vercel.json for route rewrites2

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
-
 import React from 'react';
-import { Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './components/Login';
 import Inicio from './components/Inicio';
 import EmpresaList from './components/EmpresaList'; 
@@ -12,31 +11,33 @@ import ProtectedRoute from './components/ProtectedRoute';
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Navigate to="/login" replace />} />
-      <Route path="/login" element={<Login />} />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" replace />} />
+        <Route path="/login" element={<Login />} />
 
-      <Route path="/inicio" element={
-        <ProtectedRoute><Inicio /></ProtectedRoute>
-      } />
-      <Route path="/empresas" element={
-        <ProtectedRoute><EmpresaList /></ProtectedRoute>
-      } />
-      <Route path="/productos" element={
-        <ProtectedRoute><ProductoList /></ProtectedRoute>
-      } />
-      <Route path="/inventario" element={
-        <ProtectedRoute><InventarioList /></ProtectedRoute>
-      } />
-      <Route path="/empresas/editar/:id" element={
-        <ProtectedRoute><EmpresaForm /></ProtectedRoute>
-      } />
-      <Route path="/empresas/crear" element={
-        <ProtectedRoute><EmpresaForm /></ProtectedRoute>
-      } />
-      <Route path="/productos/crear" element={
-        <ProtectedRoute><ProductoForm /></ProtectedRoute>
-      } />
-    </Routes>
+        <Route path="/inicio" element={
+          <ProtectedRoute><Inicio /></ProtectedRoute>
+        } />
+        <Route path="/empresas" element={
+          <ProtectedRoute><EmpresaList /></ProtectedRoute>
+        } />
+        <Route path="/productos" element={
+          <ProtectedRoute><ProductoList /></ProtectedRoute>
+        } />
+        <Route path="/inventario" element={
+          <ProtectedRoute><InventarioList /></ProtectedRoute>
+        } />
+        <Route path="/empresas/editar/:id" element={
+          <ProtectedRoute><EmpresaForm /></ProtectedRoute>
+        } />
+        <Route path="/empresas/crear" element={
+          <ProtectedRoute><EmpresaForm /></ProtectedRoute>
+        } />
+        <Route path="/productos/crear" element={
+          <ProtectedRoute><ProductoForm /></ProtectedRoute>
+        } />
+      </Routes>
+    </BrowserRouter>
   );
 }
